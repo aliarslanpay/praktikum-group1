3821de0c0f6754acbd5104d1338b45e5345321f3
tianmalu <tinahzcc@gmail.com>
2024-06-08 17:52:11 +0200

    assignment6..

--- assignment6/HeatForOMP/relax_jacobi.c
+++ assignment6/HeatForOMP/relax_jacobi.c
@@ -94,8 +94,8 @@ int mpi_test(int argc, char *argv[]) {
     }
 
     // Clean up
-    free_2d_array(u);
-    free_2d_array(utmp);
+    free_2d_array(u, local_nx + 2);
+    free_2d_array(utmp, local_nx + 2);
 
     MPI_Finalize();
     return 0;
@@ -138,32 +138,34 @@ double relax_jacobi( double **u1, double **utmp1,
          unsigned sizex, unsigned sizey )
 {
   int i, j;
-  double *help, *u, *utmp, factor=0.5;
+  double *help,*u, *utmp,factor=0.5;
 
-  utmp = *utmp1;
-  u = *u1;
+  utmp=*utmp1;
+  u=*u1;
   double unew, diff, sum=0.0;
 
-#pragma omp parallel for schedule (runtime) private (j, unew, diff) reduction (+:sum)
-  for(i = 1; i < sizey-1; ++i) {
-  	int ii = i * sizex;
-  	int iim1 = (i-1) * sizex;
-  	int iip1 = (i+1) * sizex;
+
+  for( i=1; i<sizey-1; i++ ) {
+  	int ii=i*sizex;
+  	int iim1=(i-1)*sizex;
+  	int iip1=(i+1)*sizex;
 #pragma ivdep
-    for( j=1; j < sizex-1; ++j){
-       unew = 0.25 * (u[ii + (j-1)]+
-        		            u[ii + (j+1)]+
-        		            u[iim1 + j]+
-        		            u[iip1 + j]);
+    for( j=1; j<sizex-1; j++ ){
+       unew = 0.25 * (u[ ii+(j-1) ]+
+        		            u[ ii+(j+1) ]+
+        		            u[ iim1+j ]+
+        		            u[ iip1+j ]);
 		    diff = unew - u[ii + j];
-		    utmp[ii + j] = unew;
+		    utmp[ii+j] = unew;
 		    sum += diff * diff;
 
        }
     }
 
-  *u1 = utmp;
-  *utmp1 = u;
-
+  *u1=utmp;
+  *utmp1=u;
   return(sum);
 }
+
+
+
